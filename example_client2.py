135e7366a93c98c8370345655166d28f7b633f53
Eloy Zuniga Jr <eloyz.email@gmail.com>
2015-09-23 10:55:45 -0500

    Converting scheduled updates to Class

--- example_client2.py
+++ example_client2.py
@@ -16,67 +16,78 @@ from aioserver.server import generate_random_color, json_encode
 STREAM_EVENTS_URL = "http://159.203.72.183:8000/events"
 SET_DATA_URL = "http://159.203.72.183:8000/data/{client_id}"
 
+# STREAM_EVENTS_URL = "http://127.0.0.1:8000/events"
+# SET_DATA_URL = "http://127.0.0.1:8000/data/{client_id}"
+
 
 logger = logging.getLogger(__name__)
 
 
-connections = 0
+class ScheduledUpdate:
 
+    def __init__(self, loop=None):
+        self._loop = loop
+        self.connections = 0
 
-@asyncio.coroutine
-def schedule(interval, loop=None):
-    """Wait for an interval to pass before proceeding.
-    This function is a coroutine.
-    """
-    logger.debug("Waiting %s seconds", interval.total_seconds())
-    yield from asyncio.sleep(interval.total_seconds(), loop=loop)
+    @property
+    def loop(self):
+        loop = self._loop
+        if loop is None:
+            loop = asyncio.get_event_loop()
+            self._loop = loop
+        return loop
 
+    @asyncio.coroutine
+    def schedule(self, interval):
+        """Wait for an interval to pass before proceeding.
+        This function is a coroutine.
+        """
+        loop = self.loop
+        logger.debug("Waiting %s seconds", interval.total_seconds())
+        yield from asyncio.sleep(interval.total_seconds(), loop=loop)
 
-@asyncio.coroutine
-def update_client(http, client_id, interval, loop=None):
-    logger.info("ADDED CLIENT %s", client_id)
-    i = 1
+    @asyncio.coroutine
+    def update_client(self, http, client_id, interval):
+        loop = self.loop
+        logger.info("ADDED CLIENT %s", client_id)
+        i = 1
 
-    global connections
+        while True:
+            yield from self.schedule(interval)
+            logger.debug("UPDATING %s", client_id)
+            data = dict(text=" Eloy {}".format(self.connections), color=generate_random_color())
+            response = yield from http.request("PUT", SET_DATA_URL.format(client_id=client_id), data=json_encode(data))
+            response.close()
+            logger.info("UPDATED %s", client_id)
+            if 200 != response.status:
+                break
+            i += 1
+        logger.info("REMOVED CLIENT %s", client_id)
 
-    while True:
-        yield from schedule(interval, loop=loop)
-        logger.debug("UPDATING %s", client_id)
-        data = dict(text=" Eloy {}".format(connections), color=generate_random_color())
-        response = yield from http.request("PUT", SET_DATA_URL.format(client_id=client_id), data=json_encode(data))
-        response.close()
-        logger.info("UPDATED %s", client_id)
-        if 200 != response.status:
-            break
-        i += 1
-    logger.info("REMOVED CLIENT %s", client_id)
+    @asyncio.coroutine
+    def start(self, interval):
+        loop = self.loop
+        http = aiohttp.ClientSession(loop=loop)
+        response = yield from http.request("GET", STREAM_EVENTS_URL)
 
+        client_id = response.headers['id']
+        logger.info('MY CONNECTION ID %s', client_id);
 
-@asyncio.coroutine
-def start(interval, loop=None):
-    http = aiohttp.ClientSession(loop=loop)
-    response = yield from http.request("GET", STREAM_EVENTS_URL)
+        clients = set()
 
-    client_id = response.headers['id']
-    logger.info('MY CONNECTION ID %s', client_id);
+        while True:
+            line = yield from response.content.readline()
+            line = line.decode('UTF-8').strip()
 
-    clients = set()
+            if line.startswith('event: created'):
+                self.connections += 1
+            elif line.startswith('event: deleted'):
+                self.connections -= 1
 
-    global connections
-
-    while True:
-        line = yield from response.content.readline()
-        line = line.decode('UTF-8').strip()
-
-        if line.startswith('event: created'):
-            connections = connections + 1
-        elif line.startswith('event: deleted'):
-            connections = connections - 1
-
-        if client_id in clients:
-            continue
-        clients.add(client_id)
-        asyncio.ensure_future(update_client(http, client_id, interval, loop=loop), loop=loop)
+            if client_id in clients:
+                continue
+            clients.add(client_id)
+            asyncio.ensure_future(self.update_client(http, client_id, interval), loop=loop)
 
 
 @click.command()
@@ -85,8 +96,10 @@ def start(interval, loop=None):
 def main(**options):
     logging.basicConfig(level=getattr(logging, options['logging'].upper()))
     loop = asyncio.get_event_loop()
+    scheduled_update = ScheduledUpdate(loop=loop)
+
     try:
-        loop.run_until_complete(start(datetime.timedelta(seconds=options['interval']), loop=loop))
+        loop.run_until_complete(scheduled_update.start(datetime.timedelta(seconds=options['interval'])))
     except KeyboardInterrupt:
         loop.stop()
     finally:
